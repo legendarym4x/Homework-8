c6f1432cb96f9612527a674e20dc9f5773bea8c3
legendarym4x <maxwel842@gmail.com>
2023-09-04 14:11:26 +0300

    upload file homework 8

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 
 
 def get_birthdays_per_week(users):
     # Реалізуйте тут домашнє завдання
-    return users
+    
+    if not users:
+        return {}
+    # Визначаємо поточну дату та тиждень після неї
+    today = date.today()
+    next_week = today + timedelta(days=7)  
+    
+    # Створюємо словники днів тижня (без вихідних) і днів народження
+    weekday = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday'}
+    birthdays_dict = {day: [] for day in weekday.values()}
+    
+    #Сортуємо дні народження згідно вимог та добавляємо іменинників до словника 
+    for user in users:
+        name = user['name'].split()[0]
+        birthday = user['birthday'].replace(year=today.year)
+        
+        if birthday < today:  
+            birthday = birthday.replace(year=today.year + 1)
+        if today <= birthday <= next_week:
+            day_of_week = birthday.strftime("%A")
+            if day_of_week not in weekday.values():
+                day_of_week = "Monday"
+            birthdays_dict[day_of_week].append(name)
+             
+    birthdays_dict = {day: names for day, names in birthdays_dict.items() if names}             
+    return birthdays_dict
+    
 
 
 if __name__ == "__main__":
